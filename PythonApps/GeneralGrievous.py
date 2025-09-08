c454ed76b479675164e8c6b7d3643031837550a3
Lampros <lmtzouvaras@gmail.com>
2025-09-08 12:58:55 +0300

    changes

---
+++ PythonApps/GeneralGrievous.py
@@ -0,0 +1,6 @@
+def answerToKenobi(KenobiSays):
+    if KenobiSays == "Hello there!":
+        return "General Kenobi!"
+
+    else:
+        return "General Skywalker, I expected someone taller."
---
+++ PythonApps/test_GeneralGrievous.py
@@ -0,0 +1,15 @@
+from testplan import test_plan
+from testplan.testing.multitest import MultiTest, testsuite, testcase
+from GeneralGrievous import answerToKenobi
+
+@testsuite
+class GeneralGrievousTests:
+
+    @testcase
+    def test_answer_to_kenobi(self, env, result):
+        result.equal(answerToKenobi('Hello there!'), "General Kenobi!")
+        result.equal(answerToKenobi('You underestimate my power!'), "General Skywalker, I expected someone taller.")
+        
+@test_plan(name="General Grievous Test Plan")
+def main(plan):
+    plan.add(MultiTest(name="General Grievous Tests", suites=[GeneralGrievousTests()]))
